120c0e4629774a130e1f324329c8f77fe3cde2fa
Sean <sdparkin@gmail.com>
2019-05-03 13:06:31 -0500

    maintenance

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+if (module.hot) {
+  module.hot.accept();
+}
+const MainApp = () => (
+  <h1>Hello React!</h1>
+);
+// render the app
+render(<MainApp />, document.getElementById('app'));
