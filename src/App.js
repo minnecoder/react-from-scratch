24e80d2e077815eec8eeac42ab88c997582e3b9e
minnecoder <sdparkin@gmail.com>
2019-05-10 15:18:44 -0500

    Changed App component to functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import hello from './hello'
+import Navbar from './Navbar'
 
-export default class App extends Component {
-  render() {
-    return (
-      // <Router>
-      //   <div>
-      //     <Navbar />
-      //   </div>
-      //   <Switch>
-      //     <Route exact path="/" component={} />
-      //   </Switch>
-      // </Router>
-      <h1>Hello</h1>
+ const App = () => (
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+        <Switch>
+          <Route exact path="/" component={hello} />
+        </Switch>
+      </Router>
     );
-  }
-}
+    
+export default App
