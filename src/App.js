e7458cd24a86b2c6ec4b05ca7d464b70a1ee3109
minnecoder <sdparkin@gmail.com>
2019-05-10 14:51:56 -0500

    made changes to fix issues with thing not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Well Hello!!</h1>
-      </div>
+      // <Router>
+      //   <div>
+      //     <Navbar />
+      //   </div>
+      //   <Switch>
+      //     <Route exact path="/" component={} />
+      //   </Switch>
+      // </Router>
+      <h1>Hello</h1>
     );
   }
 }
